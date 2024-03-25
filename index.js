696f1088402a112a36fcf08313ce0f69aac9b378
Stephen Bowen <bowensteven17@gmail.com>
2024-03-25 14:23:14 +0300

    Complete find method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+ 
+  function superbowlWin(record){
+    for(const list of record){
+      if (list.result==="W"){
+        return list.year
+      }
+    }
+      return undefined;
+  }
+  console.log(superbowlWin(record));
